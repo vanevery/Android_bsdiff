414c15b529b0e3ae007fc08ddb7bb54e6d17d5f3
vanevery <vanevery@walking-productions.com>
2011-10-11 13:48:23 -0400

    Initial Commit

---
+++ jni should have worked/bzip2/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := bzlib
+
+LOCAL_SRC_FILES := libbz.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+
+include $(PREBUILT_STATIC_LIBRARY)
